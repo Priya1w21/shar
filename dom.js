06e492cf5138f16e5f9f28dec961ca5fe0c4b65f
Priya1w21 <aarepriyanka17@gmail.com>
2023-12-05 19:33:05 +0530

    add black border in header

---
+++ dom.js
@@ -0,0 +1,4 @@
+document.getElementById("main-header").style.borderBottom="solid 4px black";
+document.getElementById("addItemsheading").style.fontWeight="bold";
+document.getElementById("addItemsheading").style.color="green";
+
